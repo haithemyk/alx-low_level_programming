eddc0188c42b9ea5969776260d5027920db72505
haithemyk <haithemyk0707@gmail.com>
2023-03-17 14:31:48 +0100

    perfect statement

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+* main - will decade if number is positive -negative -null
+*
+* Return: if the program works perfectly
+*/
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n > 0)
+printf("is positive");
+else if (n < 0)
+printf("is negative");
+else
+printf("is zero");
+}
