f192475dd265732e350d2fcd43eb1a8fa84ebd83
haithemyk <haithemyk0707@gmail.com>
2023-03-17 22:24:17 +0100

    upper case++

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - will print all alphabet letters lower - upper case
+* Return: if the program works perfectly
+*/
+int main(void)
+{
+int i;
+for (i = 97 ; i < 123 ; i++)
+{
+putchar((char) i);
+}
+for (i = 65 ; i < 91 ; i++)                     
+{                                                                              
+putchar((char) i);
+}   
+putchar('\n');
+return (0);
+}
