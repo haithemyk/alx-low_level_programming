b08cad362df8a0d3da6e43c917de1370169fc379
haithemyk <haithemyk0707@gmail.com>
2023-03-17 22:25:44 +0100

    upper case++ quic fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,10 +12,10 @@ for (i = 97 ; i < 123 ; i++)
 {
 putchar((char) i);
 }
-for (i = 65 ; i < 91 ; i++)                     
-{                                                                              
+for (i = 65 ; i < 91 ; i++)
+{
 putchar((char) i);
-}   
+}
 putchar('\n');
 return (0);
 }
