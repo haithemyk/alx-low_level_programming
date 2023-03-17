b39ddde3d730751389095b5f60012488c06c9d2e
haithemyk <haithemyk0707@gmail.com>
2023-03-17 22:34:59 +0100

    numbers are good

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - will print all numbers
+* Return: if the program works perfectly
+*/
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
