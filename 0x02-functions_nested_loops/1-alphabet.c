5951ff898b9ef4de0aa39ce9476d763d8acbf098
haithemyk <haithemyk0707@gmail.com>
2023-03-21 23:19:00 +0100

    fix problems v2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* print_alphabet - will print alphabet 
+* print_alphabet - will print alphabet
 */
 void print_alphabet(void)
 {
 int i;
-for(i = 97 ; i < 123 ; i++)
+for (i = 97 ; i < 123 ; i++)
 {
 _putchar((char) i);
 }
