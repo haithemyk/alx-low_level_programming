98dfada0fcedcce3e0c4b27487beacb1437da520
haithemyk <haithemyk0707@gmail.com>
2023-03-21 23:17:16 +0100

    fix problems

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,13 +11,3 @@ _putchar((char) i);
 }
 _putchar('\n');
 }
-
-/**
-* main - will call print_alphabet function
-* Return: if the program works perfectly
-*/
-int main(void)
-{
-print_alphabet();
-return (0);
-}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #ifndef MAIN_H
 #define MAIN_H
-int main(void);
 int _putchar(char c);
 void putschar(void);
 void print_alphabet(void);
