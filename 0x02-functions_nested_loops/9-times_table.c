a06a7dfc19c8d00da957aa3b940397eb2e303a82
haithemyk <haithemyk0707@gmail.com>
2023-03-22 02:16:40 +0100

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,17 +11,17 @@ for (i = 0 ; i < 10 ; i++)
 {
 for (j = 0 ; j < 10 ; j++)
 {
-num = i*j;
+num = i * j;
 if (j > 0)
 {
 _putchar(' ');
-if(num / 10 == 0)
+if (num / 10 == 0)
 _putchar(' ');
 }
-if(num / 10 > 0)
-_putchar(num / 10);
+if (num / 10 > 0)
+_putchar(num / 10 + '0');
 
-_putchar(num % 10);
+_putchar(num % 10 + '0');
 }
 _putchar('\n');
 }
