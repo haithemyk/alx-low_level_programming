96435534d9cc1db7a29f7d2f55d4af477cd53b8c
haithemyk <haithemyk0707@gmail.com>
2023-03-22 01:22:19 +0100

    time vc

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,13 +6,13 @@ void jack_bauer(void)
 {
 int i;
 int j;
+int first_digit;
+int second_digit;
 for (j = 0 ; j < 23 ; j++)
 {
 for (i = 0 ; i < 59 ; i++)
 {
-int first_digit;
 first_digit = j % 10;
-int second_digit;
 second_digit = j / 10;
 _putchar('0' + second_digit);
 _putchar((char) first_digit + '0');
