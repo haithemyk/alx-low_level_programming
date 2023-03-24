aff54b40f7630bbd7de9f5001abca18cc72ff1c6
haithemyk <haithemyk0707@gmail.com>
2023-03-24 02:07:27 +0100

    maybe it works

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
 #include<stdio.h>
+
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
+
 int main(void)
 {
+
 positive_or_negative(0);
 return (0);
+
 }
