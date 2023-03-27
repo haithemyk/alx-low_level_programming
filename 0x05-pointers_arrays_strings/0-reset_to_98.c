3d8c994aae29667a4e3c0b454a00c0aaad576222
haithemyk <haithemyk0707@gmail.com>
2023-03-27 15:12:11 +0100

    betty style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* swap_int - will swap two between two integers
+* reset_to_98 - will swap two between two integers
 * @n:the integer that will be trated
 */
 void reset_to_98(int *n)
