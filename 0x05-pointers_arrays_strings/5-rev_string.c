aa414c2b5d22309bbee2f31fd52cbc216f41ec72
haithemyk <haithemyk0707@gmail.com>
2023-03-27 23:30:18 +0100

    reverse not print reversed

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,16 +8,19 @@ void rev_string(char *s)
 {
 int i;
 int len;
+char temp;
 len = 0;
 while (s[len] != '\0')
 {
 len++;
 }
-i = len - 1;
-while (i >= 0)
-{
-putchar(s[i]);
-i--;
+i = 0;
+j = len - 1;
+while (i < j) {
+temp = s[i];
+s[i] = s[j];
+s[j] = temp;
+i++;
+j--;
 }
-putchar('\n');
 }
