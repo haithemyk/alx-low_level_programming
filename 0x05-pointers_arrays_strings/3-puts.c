2d32fc993d3e7144e7feb9ba456bc38a42bfb84d
haithemyk <haithemyk0707@gmail.com>
2023-03-27 15:52:28 +0100

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * _puts - print string of character similair to stdio lib
 * @str:the string that will be treated
 */
-void _puts(char *str);
+void _puts(char *str)
 {
 int i;
 i = 0;
