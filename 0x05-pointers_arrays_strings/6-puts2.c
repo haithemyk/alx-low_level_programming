47700d90196e13b9a3e253866c2567ade92a5d6e
haithemyk <haithemyk0707@gmail.com>
2023-03-27 23:41:33 +0100

    print other characters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* puts2 - print string of even indices character 
+* puts2 - print string of even indices character
 * @str:the string that will be treated
 */
 void puts2(char *str)
