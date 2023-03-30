7bbebaeafe8928edcb9649322559945b9563bdb7
haithemyk <haithemyk0707@gmail.com>
2023-03-30 02:07:16 +0100

    tired

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* reverse_array - will reverse an array
+* @a:the array that will be reversed
+* @n:the size of an array
+*/
+void reverse_array(int *a, int n)
+{
+int j;
+int i;
+i = 0;
+j = n-1;
+while (i < j)
+{
+a[i] = a[j];
+i++;
+j--;
+}
+}
