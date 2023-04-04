b809e29a4b9358640340918a769a1150c4769936
haithemyk <haithemyk0707@gmail.com>
2023-04-04 02:42:26 +0100

    sorrt betty

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _strpbrk - will search for needle in haystack
+* _strstr - will search for needle in haystack
 * @haystack:the array that we search for needle in
 * @needle:substring
 * Return: the starter point of substring match
@@ -12,7 +12,7 @@ i = 0;
 while (*haystack != '\0')
 {
 i = 0;
-while (*(haystack + i) == *(needle + i) && *(needle + i) != '\0')
+while (*(haystack + i) == *(needle + i) &&*(needle + i) != '\0')
 {
 i++;
 }
