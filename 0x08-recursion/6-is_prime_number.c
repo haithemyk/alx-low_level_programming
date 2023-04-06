c039653794a4649e18620bd8fe030179a379bffb
haithemyk <haithemyk0707@gmail.com>
2023-04-06 03:12:52 +0100

    divide and conquer

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,11 +8,11 @@ int is_prime_number(int n)
 {
 if (n <= 1)
 return (0);
-return divide(n , n-1);
+return (divide(n, n - 1));
 }
 /**
 * divide - will divide n to m
-* @n:the number will be divide 
+* @n:the number will be divide
 * @m:the number will divide to
 * Return: if m exist 0 else 1
 **/
@@ -23,5 +23,5 @@ return (1);
 else if (n % m == 0)
 return (0);
 else
-return divide(n, m - 1);  
+return (divide(n, m - 1));
 }
