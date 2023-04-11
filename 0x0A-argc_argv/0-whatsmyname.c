80ad698db9f656ab08c535ef7aa5ad54c20b0065
haithemyk <haithemyk0707@gmail.com>
2023-04-11 01:42:10 +0100

    solved

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 /**
 * mynameis - will print the string s
-* @argc:the count of element of array argv
 * @argv:the elements of arg
 * Return: return 0 means succes
 */
-int mynameis(int argc, char *argv[])
+int mynameis(char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
