24e13dc0607b5833ab431abee6d759df8a066d7b
haithemyk <haithemyk0707@gmail.com>
2023-04-11 01:40:01 +0100

    easy

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* mynameis - will print the string s
+* @argc:the count of element of array argv
+* @argv:the elements of arg
+* Return: return 0 means succes
+*/
+int mynameis(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,5 @@
+#include<stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+int mynameis(int argc, char *argv[]);
+#endif /* MAIN_H */
