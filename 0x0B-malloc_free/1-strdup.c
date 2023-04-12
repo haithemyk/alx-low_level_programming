dcd14a2062e9c4e13c8fa819910a87c4a3e700b0
haithemyk <haithemyk0707@gmail.com>
2023-04-12 02:10:49 +0100

    ?

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,16 +11,14 @@ char *_strdup(char *str)
 char *arr;
 unsigned int size;
 unsigned int i;
+if (str == NULL)
+return (NULL);
 i = 0;
 while (str[i] != '\0')
 {
 i++;
 }
 size = i;
-if (str == NULL)
-return (NULL);  
-if (size == 0)
-return (NULL);
 arr = (char *)  malloc(sizeof(char) * size);
 if (arr == NULL)
 return (NULL);
