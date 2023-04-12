64faf834c706b3004a7f7031511e8fa7eb8451ca
haithemyk <haithemyk0707@gmail.com>
2023-04-12 01:43:22 +0100

    good start v2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* _puts_recursion - will print the string s
+* create_array - will print the array of character c
 * @size:the size of buffer
 * @c: special character
+* Return: pointer to array
 */
 char *create_array(unsigned int size, char c)
 {
-char* arr;
-int i;
+char *arr;
+unsigned int i;
 if (size == 0)
-return NULL;
-arr = (char*)  malloc(sizeof(char) * size);
+return (NULL);
+arr = (char *)  malloc(sizeof(char) * size);
 for (i = 0; i < size; i++)
 {
 arr[i] = c;
 }
-return arr;
+return (arr);
 }
