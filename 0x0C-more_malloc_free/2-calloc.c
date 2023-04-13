57821f99a7a44cc723ad2d715c6654f0d30d335a
haithemyk <haithemyk0707@gmail.com>
2023-04-14 00:42:18 +0100

    should work

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _calloc - will create space in memory of size nmemb * size
+* @nmemb:number of elements
+* @size:the size of memory allocation
+* Return: pointer to first byte
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *ptr;
+if (size == 0 || nmemb == 0)
+return (NULL);
+ptr = malloc(size * nmemb);
+if (ptr == NULL)
+return (NULL);
+return (ptr);
+}
