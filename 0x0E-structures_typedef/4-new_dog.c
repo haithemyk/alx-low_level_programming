b64f62871d6b2a20dfbc0affd2f998fd619e7c8c
haithemyk <haithemyk0707@gmail.com>
2023-04-18 02:30:38 +0100

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,20 +1,39 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
 * new_dog - will create new dog_t type with paramater
 * @name: the name of dog as string
 * @owner: the owner of dog as float
 * @age: the age of dog as string
-* Return: 
+* Return: new dog_t pointer
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t d;
-d = malloc(sizeof(struct dog_t));
+dog_t *d;
+char *new_name, *new_owner;
+d = malloc(sizeof(dog_t));
 if (d  == NULL)
 return NULL;
-d->name = name;
+
+new_name = strdup(name);
+if (new_name == NULL)
+{
+free(new_name);
+return NULL;
+}
+
+new_owner = strdup(owner);
+if (new_owner == NULL) {
+free(new_name);
+free(d);
+return NULL;
+}
+
+d->name = new_name;
 d->age = age;
-d->owner = owner;
+d->owner = new_owner;
+
+return d;
 }
