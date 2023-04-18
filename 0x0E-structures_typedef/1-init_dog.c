e2e192ee40d5a3e6bd1eb033fc50852adac09e23
haithemyk <haithemyk0707@gmail.com>
2023-04-18 01:18:06 +0100

    good let's name dogs

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* init_dog - will initialize the paramater of struct dog
+* @d:the pointer of dog struct passed 
+* @name: the name of dog as string
+* @owner: the owner of dog as float
+* @age: the age of dog as string
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,4 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif /* MAIN_FILE */
