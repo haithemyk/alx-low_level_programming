f18f3c6c2f0ae59162dffbb0bc1b1e1ea31b795e
haithemyk <haithemyk0707@gmail.com>
2023-04-20 00:17:15 +0100

    call iterations

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* print_name - will call f to print name
+* @array: the pointer to integer array
+* @size: the size of array
+* @action: the pointer of function
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned long i;
+if (array == NULL || action == NULL || size == 0)
+return;
+for (i = 0; i < size; i++)
+(*action)(array[i]); 
+}
