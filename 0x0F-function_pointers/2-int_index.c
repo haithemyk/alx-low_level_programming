cca6f25bacd46b0258839b031e57e74f18b598a9
haithemyk <haithemyk0707@gmail.com>
2023-04-20 00:53:29 +0100

    work

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+* int_index - will call cmp to print array index that match cmp parameter
+* @array: the pointer to integer array
+* @size: the size of array
+* @cmp: the pointer of function
+* Return:the index if cmp match -1 otherwise
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i, r;
+if (array == NULL || action == NULL || size == 0)
+return (-1);
+for (i = 0; i < size; i++)
+{
+r = (*cmp)(array[i]);
+if (r != 0)
+return (i);
+}
+return (-1);
+}
