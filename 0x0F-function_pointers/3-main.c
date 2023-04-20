eb621a9823191120f5b044e5efcdc1dab8b1e93e
haithemyk <haithemyk0707@gmail.com>
2023-04-20 02:52:38 +0100

    calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+
+/**
+* main - check the code
+* @argc:the size of argv
+* @argv:arguments array
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+char *o;
+int a ,b;
+int (*op_ptr)(int a, int b) ;
+if (argc != 4)
+{
+printf("Error/n");
+return (98);
+}
+a = atoi(argv[1]);
+o = argv[2];
+b = atoi(argv[3]);
+if (b == 0 && strcmp(o,"/") == 0)
+{
+printf("Error\n");
+return (100);
+}
+op_ptr = get_op_func(o);
+if (op_ptr == NULL)
+{
+printf("Error\n");
+return (99);
+}
+printf("%d", op_ptr(a,b));
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+/**
+* op_add - will calculate sum of a and b
+* @a: the first arg as integer
+* @b: the size of array
+* Return:the sum of a and b
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+* op_sub - will calculate substitution of a and b
+* @a: the first arg as integer
+* @b: the size of array
+* Return:the sub of a and b
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+* op_mul - will calculate multiplucation of a and b
+* @a: the first arg as integer
+* @b: the second arg as integer
+* Return:the mul of a and b
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+* op_mod - will calculate modulo of a and b
+* @a: the first arg as integer
+* @b: the second arg as integer
+* Return:the mod of a and b
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
+
+/**
+* op_div - will calculate division of a and b
+* @a: the first arg as integer
+* @b: the second arg as integer
+* Return:the div of a and b
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
