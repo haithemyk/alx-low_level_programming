f1be9385ee2d1219dffb3f6d09dd0bfc2c509c58
haithemyk <haithemyk0707@gmail.com>
2023-04-20 22:31:43 +0100

    work pls

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+/**
+* print_numbers - will print all args seperated by separator
+* @n: the number of args
+* @separator: string will seperate between args
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list ap;
+unsigned int i;
+char *new_separator;
+if (n == 0)
+return;
+if (separator == NULL)
+{
+new_separator = malloc(1);
+new_separator[0] = '\0';
+}
+new_separator = malloc(strlen(separator) + 1);
+strncpy(new_separator, separator, strlen(separator) + 1);
+va_start(ap, n);
+for (i = 0; i < n; i++)
+{
+if (i < n - 1)
+printf("%d%s", va_arg(ap, int), new_separator);
+else
+printf("%d", va_arg(ap, int));
+}
+va_end(ap);
+putchar('\n');
+}
