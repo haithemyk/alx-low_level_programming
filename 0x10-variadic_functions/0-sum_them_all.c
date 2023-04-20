db7ccfb0f49355b333a17ee63b8b7670fbe701b0
haithemyk <haithemyk0707@gmail.com>
2023-04-20 22:03:45 +0100

    no problem

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,4 @@
-#include "function_pointers.h"
+#include "variadic_functions.h"
 /**
 * sum_them_all - will return the sum of all args passed
 * @n: the number of args
@@ -11,10 +11,10 @@ return (0);
 va_list ap;
 int i, sum;
 sum = 0;
-va_start(ap, n); 
+va_start(ap, n);
 for (i = 0; i < n; i++)
 {
-sum += va_args(ap,int);
+sum += va_args(ap, int);
 }
 va_end(ap);
 return (sum);
