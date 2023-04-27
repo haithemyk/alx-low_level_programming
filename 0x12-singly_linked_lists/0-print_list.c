44933624201dfd8c7d51e809237e49c6437cfff8
haithemyk <haithemyk0707@gmail.com>
2023-04-27 23:30:24 +0100

    will work

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_list - will print the list from the head to tail
+ * @h:list that will be loop through as argument
+ * Return:the size of list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t size;
+	const list_t *ptr;
+	ptr = h;
+	size = 0;
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+			printf("[0] (Nil)\n");
+		else
+			printf("[%i] %s\n", ptr->len, ptr->str );
+
+		ptr = ptr->next;
+		size++;
+	}
+	return size;
+
+}
