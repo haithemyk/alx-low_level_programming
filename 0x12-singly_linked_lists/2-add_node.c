df2a0f3add099a05f17b3b6318ff7c6e7226685a
haithemyk <haithemyk0707@gmail.com>
2023-04-28 00:31:01 +0100

    null if failed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,17 @@
 #include "lists.h"
 /**
- * print_list - will print the list from the head to tail
- * @h:list that will be loop through as argument
- * Return:the size of list
+ * add_node - will add node at the head of list
+ * @head:the head of list may be null!
+ * @str:the string of struct list_t
+ * Return:the pointer to the new head of list
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *ptr;
+
 	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+		return (NULL);
 	ptr->str = strdup(str);
 	ptr->len = strlen(str);
 	ptr->next = NULL;
