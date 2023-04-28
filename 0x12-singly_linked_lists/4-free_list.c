c67195ce5960c9c11707203567e04faa3f89aaf8
haithemyk <haithemyk0707@gmail.com>
2023-04-28 01:24:22 +0100

    fixed ptr issue

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,17 @@
 #include "lists.h"
 /**
- * add_node_end - will add node at the tail of list
+ * free_list - will free all nodes
  * @head:the head of list may be null!
- * @str:the string of struct list_t
- * Return:the pointer to the new tail of list
  */
 void free_list(list_t *head)
 {
 	list_t *trav;
-	trav = *head;
+	trav = head;
 	while (trav->next != NULL)
 	{
 		trav = trav->next;
-		free(*head);
-		*head = trav;	
+		free(head);
+		head = trav;	
 	}
-	free(*head);
+	free(head);
 }
