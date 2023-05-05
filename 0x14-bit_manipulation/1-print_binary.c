b54328362f95f9a1ee8d6923d003dc6078994f6b
haithemyk <haithemyk0707@gmail.com>
2023-05-05 02:09:11 +0100

    print binary stream without modulo or arrays

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_binary - print binary number.
+* @n:the integer that will be treated.
+*/
+void print_binary(unsigned long int n)
+{
+unsigned int curr;
+int i;
+i = 0;
+curr = n;
+while (curr != 0)
+{
+	while (curr > 1)
+		curr -= 2;
+	_putchar(curr);
+	curr = n >> 1;
+}
+}
