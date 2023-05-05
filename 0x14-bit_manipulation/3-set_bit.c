9c52ecd957278ee9201cdc19386780019c239d7c
haithemyk <haithemyk0707@gmail.com>
2023-05-05 23:51:32 +0100

    should work

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
-* get_bit - get bit at certain index
+* set_bit - get bit at certain index
 * @index: the index of wanted bit
 * @n: the number that will be treated
 * Return:the bit
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (n >> index == 0)
-		return (-1);
-   
-    return (n >> index) & 1;
+    if (index >= sizeof(unsigned long int)
+		    return -1;
+    *n |= (1ul << index);
+    return 1;
+}
 
 }
