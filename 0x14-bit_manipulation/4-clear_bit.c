2743af0cc9738cab84c765280d95378e740986ae
haithemyk <haithemyk0707@gmail.com>
2023-05-06 00:21:18 +0100

    clear

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,7 +4,7 @@
 * @n: the number that will be treated.
 * Return:1 if succed -1 else.
 */
-int clear_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int cp;
 	cp = *n;
