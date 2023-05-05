b8d9e1d9e0f72986e30254c1ad796fefcd756819
haithemyk <haithemyk0707@gmail.com>
2023-05-05 23:59:57 +0100

    fix all the problems

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,15 +2,13 @@
 /**
 * set_bit - get bit at certain index
 * @index: the index of wanted bit
-* @n: the number that will be treated
+* @n: the pointer to the number that will be treated
 * Return:the bit
 */
-int get_bit(unsigned long int *n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-    if (index >= sizeof(unsigned long int))
-		    return (-1);
-    *n |= (1 << index);
-    return (1);
-}
-
+	if (index >= sizeof(unsigned long int))
+		return (-1);
+	*n |= (1 << index);
+	return (1);
 }
