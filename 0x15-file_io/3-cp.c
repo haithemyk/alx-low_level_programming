fc5b27b1cd8cc7ee0f590678cc20155bc7b7e67c
haithemyk <haithemyk0707@gmail.com>
2023-05-09 01:46:25 +0100

    big brains always win

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,8 @@ int main(int argc, char *argv[])
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+argv[1]), exit(98);
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
@@ -29,7 +30,8 @@ int main(int argc, char *argv[])
 	do {
 		n_read = read(fd_from, buf, BUF_SIZE);
 		if (n_read == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+argv[1]), exit(98);
 		n_written = write(fd_to, buf, n_read);
 		if (n_written == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
